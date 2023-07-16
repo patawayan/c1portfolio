d731a9ac6308dd11af432a5f44568b4b12b177c4
patawayan <pawayan3@gmail.com>
2023-07-17 04:10:42 +0800

    Init resizable Navbar

--- src/components/navbar/NavBar.vue
+++ src/components/navbar/NavBar.vue
@@ -7,7 +7,10 @@ const minimized = ref(false);
 </script>
 
 <template>
-  <div class="w-3/12 h-full dark:bg-white-10 p-5 flex-col">
-    <NavBarMenu :page-data="pages" :minimized="minimized" />
+  <div class="w-3/12 h-full flex">
+    <div class="h-full w-full dark:bg-white-10 p-5 flex flex-col">
+      <NavBarMenu :page-data="pages" :minimized="minimized" />
+    </div>
+    <div class="min-w-[5px] h-full bg-white-60 cursor-pointer"></div>
   </div>
 </template>
